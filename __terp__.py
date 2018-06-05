8be0d33d8acb96cbe196194eebcc1439351e867d
Eduard Carreras <ecarreras@gisce.net>
2018-06-05 16:04:19 +0200

    Bump to v0.10.0

--- __terp__.py
+++ __terp__.py
@@ -8,7 +8,7 @@
     NOTE: Needs poweremail with conversations suport.
           See: https://github.com/openlabs/poweremail/issues/24
     """,
-    "version": "0.9.0",
+    "version": "0.10.0",
     "author": "GISCE-TI",
     "category": "CRM",
     "depends":[
