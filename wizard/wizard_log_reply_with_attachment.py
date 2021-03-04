e8670ee1a609c7459f1e6af7bb8f67526063a937
Guillem Julià <gjulia@gisce.net>
2021-03-05 00:16:48 +0100

    Add limit size validation

--- wizard/wizard_log_reply_with_attachment.py
+++ wizard/wizard_log_reply_with_attachment.py
@@ -30,19 +30,25 @@ class WizardLogReplyAttachment(osv.osv_memory):
         'info': lambda *a: _('Select a file to send as an attachment')
     }
 
+
     def log_reply(self, cursor, uid, ids, context=None):
         if context is None:
             context = {}
         case_obj = self.pool.get('crm.case')
+        conf_obj = self.pool.get('res.config')
         attachment_obj = self.pool.get('ir.attachment')
         wiz = self.browse(cursor, uid, ids[0], context=context)
 
         ctx = context.copy()
         attachment_ids = []
+        total_size = 0
+        max_mb_size = float(conf_obj.get(
+            cursor, uid, 'crm_poweremail_max_size_mb', 20))
         for x in xrange(1, 6):
             file_content = getattr(wiz, 'file_{}'.format(x))
             filename = getattr(wiz, 'filename_{}'.format(x))
             if file_content:
+                total_size += (len(file_content) * (3.0 / 4.0)) - file_content.count('=', -2)
                 attachment_data = {
                     'name': filename,
                     'content': file_content
@@ -58,6 +64,14 @@ class WizardLogReplyAttachment(osv.osv_memory):
                     'res_id': wiz.case_id.id
                 })
                 attachment_ids.append(att_id)
+        megas = round(float(total_size)/(1024*1024), 2)
+        if megas > max_mb_size:
+            raise osv.except_osv(
+                _(u"User error"),
+                _(
+                    u"You are trying to send {}MB. The limit is {}MB"
+                ).format(megas, max_mb_size)
+            )
         ctx['attachment_ids'] = attachment_ids
         case_obj.case_log_reply(
                 cursor, uid, [wiz.case_id.id], context=ctx
