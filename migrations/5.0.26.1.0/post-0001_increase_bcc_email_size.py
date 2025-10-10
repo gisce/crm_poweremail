a009e0fb6e228f9f4b8a167085b6284a536fb8ce
Yisus <sgutierrez@gisce.net>
2025-10-10 15:00:05 +0200

    Feat: increase the secret email field size

---
+++ migrations/5.0.26.1.0/post-0001_increase_bcc_email_size.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from oopgrade.oopgrade import MigrationHelper
+
+def up(cursor, installed_version):
+    if not installed_version:
+        return
+
+    module = 'crm_poweremail'
+    mh = MigrationHelper(cursor, module)
+    sql = """
+        ALTER TABLE crm_case
+            ALTER COLUMN email_bcc TYPE varchar(1000);
+    """
+    cursor.execute(sql)
+
+    return True
+
+
+def down(cursor, installed_version):
+    pass
+
+
+migrate = up
+
